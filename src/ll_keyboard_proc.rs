55cabde0980ece36812bc87364072af814c462ed
emptylsjl <songjiellarry@gmail.com>
2022-04-18 23:47:07 -0700

    separate llkp hook and keyboard struct

--- src/ll_keyboard_proc.rs
+++ src/ll_keyboard_proc.rs
@@ -1,7 +1,6 @@
-use std::borrow::Borrow;
-use std::cell::RefCell;
+
+use std::cell::{Ref, RefCell};
 use std::collections::{HashMap, VecDeque};
-use std::collections::hash_map::Keys;
 use std::lazy::Lazy;
 use std::os::raw::{c_int, c_long};
 use std::process;
@@ -9,16 +8,14 @@ use std::ptr::null_mut;
 
 use winapi::shared::minwindef::{DWORD, HINSTANCE, LPARAM, LRESULT, UINT, WPARAM};
 use winapi::shared::windef::{HHOOK, HWND, POINT};
-use winapi::um::winuser::{CallNextHookEx, GetMessageW, HOOKPROC, KBDLLHOOKSTRUCT, MSG, SetWindowsHookExW, UnhookWindowsHookEx};
+use winapi::um::winuser::{CallNextHookEx, GetMessageA, HOOKPROC, KBDLLHOOKSTRUCT, MSG, SetWindowsHookExA, UnhookWindowsHookEx};
 
 use crate::wm_vkey::{WM_VKEY, WM_VKEY::*, WM_KEYSTATUS, WM_KEYSTATUS::*, KeyStatus};
 
-
 const WH_KEYBOARD_LL: c_int = 13;
 
-static mut LLKP: Lazy<RefCell<LLKeyboard>> = Lazy::new(|| {
-    RefCell::new(LLKeyboard::new())
-});
+static mut LLKP: Lazy<RefCell<LLKeyboard>> = Lazy::new(|| { RefCell::new(LLKeyboard::new()) });
+static mut LLKP_HOOK: Option<HHOOK> = None;
 
 #[derive(Debug, Default, Clone)]
 struct Keystroke {
@@ -45,28 +42,46 @@ impl PartialEq for Keystroke {
 
 struct KeyEvent {
     consume: bool,
-    event: Box<dyn Fn() + 'static>,
+    event: Box<dyn Fn(Ref<LLKeyboard>) + 'static>,
+}
+
+impl Default for KeyEvent {
+    fn default() -> Self {
+        KeyEvent {
+            consume: false,
+            event: Box::new(|_| {})
+        }
+    }
 }
 
 struct LLKeyboard {
-    hhk: Option<HHOOK>,
     events: HashMap<WM_VKEY, KeyEvent>,
     records: VecDeque<Keystroke>,
     records_u128: u128,
 }
 
+// impl Clone for LLKeyboard {
+//     fn clone(&self) -> Self {
+//         LLKeyboard {
+//             hhk: None,
+//             events: ,
+//             records: Default::default(),
+//             records_u128: 0
+//         }
+//     }
+// }
+
 impl LLKeyboard {
 
     fn new() -> LLKeyboard {
         LLKeyboard {
-            hhk: None,
             events: HashMap::with_capacity(0xff),
             records: VecDeque::from(vec![Keystroke::default(); 10]),
             records_u128: 0
         }
     }
 
-    fn add_event<F: Fn() + 'static>(&mut self, key: WM_VKEY, event: F, consume: bool) {
+    fn add_event<F: Fn(Ref<LLKeyboard>) + 'static>(&mut self, key: WM_VKEY, event: F, consume: bool) {
         let event = Box::new(event);
         self.events.insert(
             key,
@@ -110,21 +125,18 @@ impl LLKeyboard {
         };
         records
     }
+}
 
+unsafe fn llkp_hook(keyboard_proc: HOOKPROC) {
+    LLKP_HOOK = Some(SetWindowsHookExA(WH_KEYBOARD_LL, keyboard_proc, 0 as HINSTANCE, 0 as DWORD));
+    // UnhookWindowsHookEx(self.hhk.unwrap());
+}
 
-    unsafe fn hook(&mut self, keyboard_proc: HOOKPROC) {
-        self.hhk = Some(SetWindowsHookExW(WH_KEYBOARD_LL, keyboard_proc, 0 as HINSTANCE, 0 as DWORD));
-        // UnhookWindowsHookEx(self.hhk.unwrap());
+unsafe fn llkp_unhook() {
+    if !LLKP_HOOK.is_none() {
+        UnhookWindowsHookEx(LLKP_HOOK.unwrap());
     }
-
-
-    unsafe fn unhook(&mut self) {
-        if !self.hhk.is_none() {
-            UnhookWindowsHookEx(self.hhk.unwrap());
-        }
-        self.hhk = None;
-        // UnhookWindowsHookEx(self.hhk.unwrap());
-    }
+    LLKP_HOOK = None;
 }
 
 fn get_message() {
@@ -137,26 +149,39 @@ fn get_message() {
             time : 0 as DWORD,
             pt : POINT { x: 0 as c_long, y: 0 as c_long, },
         };
-        GetMessageW(&mut msg, 0 as HWND, 0 as UINT, 0 as UINT);
+        GetMessageA(&mut msg, 0 as HWND, 0 as UINT, 0 as UINT);
     }
 }
 
 unsafe extern "system" fn ll_keyboard_proc(code: c_int, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    let KBDLLHOOKSTRUCT { vkCode, scanCode, time, .. } = *(l_param as *const KBDLLHOOKSTRUCT);
+    let KBDLLHOOKSTRUCT {
+        vkCode, scanCode, flags, time, dwExtraInfo
+    } = *(l_param as *const KBDLLHOOKSTRUCT);
 
     let key = WM_VKEY::from(vkCode);
     let status = KeyStatus::from(w_param as u32);
+    println!("{key:?}");
     LLKP.borrow_mut().record_key(key, status);
 
-    if key == RSHIFT {
-        LLKP.borrow_mut().unhook();
-        process::exit(0);
+    // key_event = LLKP.borrow_mut().events.get(&key).unwrap_or(key_event);
+    // let llkp = ;
+    // (key_event.event)();
+    // if key_event.consume { return 1; }
+    // let mut key_event = &KeyEvent::default();
+    // let key_event = LLKP.borrow_mut().events.get(&key).unwrap_or(key_event);
+    // {
+    //     let a = LLKP.borrow_mut();
+    //     let b = a.events.get(&key).unwrap_or(key_event);
+    //     key_event = (*b).
+    // }
+
+    // let event = ;
+    if let Some(key_event) = LLKP.borrow().events.get(&key) {
+        (key_event.event)(LLKP.borrow());
+        if key_event.consume { return 1; }
     }
 
-    if let Some(key_event) = LLKP.borrow_mut().events.get(&key) {
-        (key_event.event)();
-        if key_event.consume { return 1; }
-    };
+
     CallNextHookEx(null_mut(), code, w_param, l_param)
 }
 
@@ -164,9 +189,11 @@ unsafe extern "system" fn ll_keyboard_proc(code: c_int, w_param: WPARAM, l_param
 pub fn init() {
     unsafe {
         let x = 5;
-        LLKP.borrow_mut().add_event(LSHIFT,move || { println!("{}", x);}, true);
+        LLKP.borrow_mut().add_event(KEY_A,move |_| { println!("{}", x);}, true);
+        LLKP.borrow_mut().add_event(KEY_B, |llkp| { llkp.get_records().iter().for_each(|x| { println!("{:?}-{:?} ", x.key, x.status);});}, true);
+        LLKP.borrow_mut().add_event(RSHIFT, |_| { llkp_unhook(); process::exit(0); }, true);
 
-        LLKP.borrow_mut().hook(Some(ll_keyboard_proc));
+        llkp_hook(Some(ll_keyboard_proc));
         get_message();
     }
 }
